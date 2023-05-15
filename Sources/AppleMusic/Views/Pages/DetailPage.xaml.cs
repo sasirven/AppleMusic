79a4b8bec59f92e8433cc440dfc25cee19bafa77
Samuel SIRVEN <samuel.sirven@etu.uca.fr>
2023-05-15 04:03:34 +0200

    💄add the first custom view
    - ImageTitleView for handle the image and album title

---
+++ Sources/AppleMusic/Views/Pages/DetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppleMusic.Views.Pages;
+
+public partial class DetailPage : ContentPage
+{
+    public DetailPage()
+    {
+        InitializeComponent();
+    }
+}
