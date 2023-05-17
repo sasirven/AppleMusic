3caac0a45e7704576703180d77931f0f4444360b
Samuel SIRVEN <samuel.sirven@etu.uca.fr>
2023-05-17 02:14:57 +0200

    👔 add the manager to the app

--- Sources/AppleMusic/App.xaml.cs
+++ Sources/AppleMusic/App.xaml.cs
@@ -1,9 +1,12 @@
 ﻿using AppleMusic.Views.CustomViews;
+using Model;
+using StubLib;
 
 namespace AppleMusic;
 
 public partial class App : Application
 {
+	public Manager MyManager { get; } = new(new Stub());
 	public App()
 	{
 		InitializeComponent();
