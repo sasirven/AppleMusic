4bac8c97d30ef345223b78d18b1515aace9bf364
Samuel SIRVEN <samuel.sirven@etu.uca.fr>
2023-05-17 02:16:00 +0200

    💄 update image title view
    - Update the view with binding
    
    ⚠️ Binding not work for now (Build but no display)

--- Sources/AppleMusic/Views/CustomViews/ImageTitleView.xaml.cs
+++ Sources/AppleMusic/Views/CustomViews/ImageTitleView.xaml.cs
@@ -1,15 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+
+using Model;
 
 namespace AppleMusic.Views.CustomViews;
 
 public partial class ImageTitleView : ContentView
 {
+    public Album Album
+    {
+        get => (Album)GetValue(AlbumProperty);
+        set => SetValue(AlbumProperty, value);
+    }
+
+    public static readonly BindableProperty AlbumProperty =
+        BindableProperty.Create(nameof(Album), typeof(Album), typeof(ImageTitleView), null, BindingMode.OneTime);
+    
     public ImageTitleView()
     {
         InitializeComponent();
+        BindingContext = Album;
     }
 }
