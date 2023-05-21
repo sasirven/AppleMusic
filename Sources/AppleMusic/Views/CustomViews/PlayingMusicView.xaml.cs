c1f00656e33d16967f1e073d5c2b0d41926746cb
Samuel SIRVEN <samuel.sirven@etu.uca.fr>
2023-05-21 23:53:37 +0200

    💄 add playing music view

---
+++ Sources/AppleMusic/Views/CustomViews/PlayingMusicView.xaml.cs
@@ -0,0 +1,16 @@
+﻿
+
+using Model;
+
+namespace AppleMusic.Views.CustomViews;
+
+public partial class PlayingMusicView : ContentView
+{
+    public Manager Manager => ((App)Application.Current!).MyManager;
+    
+    public PlayingMusicView()
+    {
+        InitializeComponent();
+        BindingContext = Manager;
+    }
+}
