a4c702089c543bf82fd791cd4fd6f7308c63481d
Samuel SIRVEN <samuel.sirven@etu.uca.fr>
2023-05-21 21:21:44 +0200

    💄 add the podcast navbar

---
+++ Sources/AppleMusic/Views/CustomViews/AlbumDetail/AlbumNavBarView.xaml.cs
@@ -0,0 +1,23 @@
+﻿
+namespace AppleMusic.Views.CustomViews.AlbumDetail;
+
+public partial class AlbumNavBarView : ContentView
+{
+    public string AlbumName
+    {
+        get => (string)GetValue(AlbumNameProperty);
+        set => SetValue(AlbumNameProperty, value);
+    }
+    public static readonly BindableProperty AlbumNameProperty =
+        BindableProperty.Create(nameof(AlbumName), typeof(string), typeof(AlbumNavBarView), string.Empty, BindingMode.OneTime);
+    
+    public AlbumNavBarView()
+    {
+        InitializeComponent();
+    }
+
+    private void ImageButton_Clicked(object? sender, EventArgs e)
+    {
+        Navigation.PopAsync();
+    }
+}
