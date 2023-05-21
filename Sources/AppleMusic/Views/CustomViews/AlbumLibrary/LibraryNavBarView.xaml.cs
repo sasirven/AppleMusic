fd7968026f54693262803026959ade0c1cdc92d0
Samuel SIRVEN <samuel.sirven@etu.uca.fr>
2023-05-21 21:21:36 +0200

    💄 add the library navbar

---
+++ Sources/AppleMusic/Views/CustomViews/AlbumLibrary/LibraryNavBarView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppleMusic.Views.CustomViews.AlbumLibrary;
+
+public partial class LibraryNavBarView : ContentView
+{
+    public LibraryNavBarView()
+    {
+        InitializeComponent();
+    }
+}
